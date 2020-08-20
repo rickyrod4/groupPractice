db4fdcbd8e11932471f7b592fb7ff9471dba8057
Ricky Rodriguez <ricardo.rodriguez4@yahoo.com>
2020-08-19 22:33:33 -0500

    Added registration/validations

--- home/models.py
+++ home/models.py
@@ -6,6 +6,53 @@ import re
 EMAIL_MATCH = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class UserManager(models.Manager):
+    #Validating the User Registration Information
+    def validations(self, form_data):
+        errors = {}
+        if len(form_data['first_name']) < 1:
+            errors['first_name'] = 'Must enter a first name' 
+
+        if len(form_data['last_name']) < 1:
+            errors['last_name'] = 'Must enter a last name' 
+        
+        if len(form_data['email']) < 1:
+            errors['email'] = 'Must enter an email'
+        
+        if len(form_data['password']) < 1:
+            errors['password'] = 'Must enter an password'
+        
+        if len(form_data['confirm']) < 1:
+            errors['confirm'] = 'Must confirm your password'
+        
+        if form_data['password'] != form_data['confirm']:
+            errors['confirm'] = 'Passwords do not match'   
+        
+        if not EMAIL_MATCH.match(form_data['email']):
+            errors['email'] = 'Invalid Email'
+        
+        users_with_email = self.filter(email=form_data['email'])
+        if users_with_email:
+            errors['email'] = 'Email Is Already Registered'                 
+        return errors
+    #Registering A User
+    def register(self, form_data):
+        hash1 = bcrypt.hashpw(form_data['password'].encode(),bcrypt.gensalt()).decode()
+        self.create(
+            first_name = form_data['first_name'],
+            last_name = form_data['last_name'],
+            email = form_data['email'],
+            password = hash1
+        )
+    #Authenticating Login Information
+    def authenticate(self, email, password):
+        users_with_emails = self.filter(email = email)
+        if not users_with_emails:
+            return False
+        user = users_with_emails[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+
+
 
 
 # Create your models here.
